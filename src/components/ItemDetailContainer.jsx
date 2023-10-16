f30f1c03183d9537ab04de3d3c05c0451d517c17
Josue <joshvlbn@gmail.com>
2023-10-15 21:47:56 -0300

    prueba

--- src/components/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer.jsx
@@ -23,7 +23,7 @@ const ItemDetailContainer = () => {
         } catch (error) {
             console.error(error)
         }
-    }, []);
+    }, [itemParam.id]);
 
     useEffect(() => {
         getData();
